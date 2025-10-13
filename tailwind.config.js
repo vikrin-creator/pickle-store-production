e8bb972014966948cbdda72f07849053cc303c28
Vinod <vinod@picklestore.com>
2025-10-13 17:30:56 +0530

    Fix: Remove white space and make website fully responsive for all screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+        '3xl': '1600px',
+        '4xl': '2000px',
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+        '128': '32rem',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [],
 }
