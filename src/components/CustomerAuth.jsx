910be0da758aae7826a68f6dc76c1f55b24f7ab4
Vinod <vinod@picklestore.com>
2025-10-23 18:30:58 +0530

    Update CustomerAuth.jsx changes

--- src/components/CustomerAuth.jsx
+++ src/components/CustomerAuth.jsx
@@ -155,7 +155,7 @@ const CustomerAuth = ({ onClose, onSuccess }) => {
                 >
                   <path d="M13.8261 17.4264C16.7203 18.1174 20.2244 18.5217 24 18.5217C27.7756 18.5217 31.2797 18.1174 34.1739 17.4264C36.9144 16.7722 39.9967 15.2331 41.3563 14.1648L24.8486 40.6391C24.4571 41.267 23.5429 41.267 23.1514 40.6391L6.64374 14.1648C8.00331 15.2331 11.0856 16.7722 13.8261 17.4264Z" fill="currentColor"></path>
                 </svg>
-                <h1 className="text-lg sm:text-xl font-bold tracking-tight">Janiitra Pickles</h1>
+                {/* <h1 className="text-lg sm:text-xl font-bold tracking-tight">Janiitra Pickles</h1> */}
               </div>
               {/* Close Button */}
               <button
